4927b2d217a711addc8485095e8f135e9e19f535
alireza.zarinjoee <167171532+alirezaZarinjoee@users.noreply.github.com>
2024-05-18 12:37:31 -0700

    Update settings.py

--- task/task/settings.py
+++ task/task/settings.py
@@ -143,8 +143,8 @@ AUTH_USER_MODEL='accounting.CustomUser'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'alirezadjango2004@gmail.com'
-EMAIL_HOST_PASSWORD = 'nclh gcjb vyql pgae'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
